db03b4d9c12856fb11226482019ce12ac87f28cd
Matsonj30 <matsonj2013@gmail.com>
2023-04-11 15:30:54 -0600

    buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,19 @@
 function burgerMenu(){
-    var checkbox = document.getElementById("burgerCheckbox");
+    var checkbox = document.getElementById("hamburgerCheckbox");
     var menu = document.getElementById("menu");
-    
+    var burgerMenu = document.getElementsByClassName("hamburgerMenu");
+
     if(checkbox.checked == true){
         menu.style.transform = "none";
+        burgerMenu[0].style.position = "fixed";
     }
     else{
         menu.style.transform = "translate(200px)";
+        burgerMenu[0].style.position = "absolute";
     }
 }
+
+
 
 function scrollToDiv(location){
     document.getElementById(location).scrollIntoView({behaviour: "smooth"});
