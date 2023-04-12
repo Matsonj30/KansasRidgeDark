ab50ea19773020d3b5e9a608f14b8195d4443fae
Matsonj30 <matsonj2013@gmail.com>
2023-04-12 15:53:24 -0600

    fixes

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,52 @@ function scrollToDiv(location){
     document.getElementById(location).scrollIntoView({behaviour: "smooth"});
 
 }
+
+
+
+//scroll events still trigger on button clicks, not manual scrolling (any scrolling of any event)
+document.getElementById("imageContainer").addEventListener("scroll", scrollEvent);
+
+function changeIndex(direction){
+
+    var currentIndex = getCurrentIndex()
+    //current spot +- 1 depending on button clicked
+    nextIndex = currentIndex + direction;
+    if(nextIndex == -1){
+        nextIndex = 4;
+    }
+    else if(nextIndex == 5){
+        nextIndex = 0;
+    }
+   
+    var nextDestination = document.getElementById("image"+ (nextIndex)); 
+    nextDestination.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+
+}
+function getCurrentIndex(){
+    scrollContainer = document.getElementById("imageContainer");
+    var indicies = scrollContainer.getElementsByClassName("image");
+
+    var positionInContainer = scrollContainer.scrollLeft;
+    var widthOfTestimonial = indicies[0].offsetWidth; //all same width
+    var currentIndex = Math.floor(positionInContainer / widthOfTestimonial);
+    //If you add too much padding, it may get the wrong index if you add a lot of elements
+    return currentIndex;
+}
+
+//cant put this in the scroll event listener for some reason
+function scrollEvent(){
+    changeCircleColor(getCurrentIndex())
+}
+function changeCircleColor(currentIndex){
+    for(i = 0; i <= 4; i+=1){
+        var circle = document.getElementById("circle"+i)
+        if(currentIndex == i){
+            circle.style.backgroundColor = "#fafafa"
+        }
+        else{
+            circle.style.backgroundColor = "#bdbdbd"
+        }
+    }
+
+}
